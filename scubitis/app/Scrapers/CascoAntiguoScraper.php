594ab3d4a45e610352f20ce94f1d3928d7a3e458
jordiprats <jordi.prats@gmail.com>
2018-11-02 19:26:27 +0100

    TODO meta

--- scubitis/app/Scrapers/CascoAntiguoScraper.php
+++ scubitis/app/Scrapers/CascoAntiguoScraper.php
@@ -6,6 +6,7 @@ class CascoAntiguoScraper implements Scraper
 {
   public function getWebPrice($url)
   {
+    //http://php.net/manual/en/function.get-meta-tags.php
     $c = curl_init($url);
     curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($c, CURLOPT_USERAGENT,'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0');
