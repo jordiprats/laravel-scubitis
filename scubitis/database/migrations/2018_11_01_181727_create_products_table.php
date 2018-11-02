0fb65a2e3c07f9e95d4db58f7be57fa547b1a35f
jordiprats <jordi.prats@gmail.com>
2018-11-02 17:55:50 +0100

    lint

--- scubitis/database/migrations/2018_11_01_181727_create_products_table.php
+++ scubitis/database/migrations/2018_11_01_181727_create_products_table.php
@@ -6,28 +6,28 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProductsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-      Schema::create('products', function (Blueprint $table) {
-        $table->increments('id');
-        $table->string('description');
-        $table->integer('category_id')->references('id')->on('categories');
-        $table->timestamps();
-      });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('products', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('description');
+      $table->integer('category_id')->references('id')->on('categories');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-      Schema::dropIfExists('products');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('products');
+  }
 }
--- scubitis/database/migrations/2018_11_01_181747_create_web_prices_table.php
+++ scubitis/database/migrations/2018_11_01_181747_create_web_prices_table.php
@@ -6,29 +6,29 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateWebPricesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-      Schema::create('web_prices', function (Blueprint $table) {
-        $table->increments('id');
-        $table->string('url');
-        $table->decimal('price', 5, 2);
-        $table->integer('product_id')->references('id')->on('products');
-        $table->timestamps();
-      });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('web_prices', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('url');
+      $table->decimal('price', 5, 2);
+      $table->integer('product_id')->references('id')->on('products');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-      Schema::dropIfExists('web_prices');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('web_prices');
+  }
 }
