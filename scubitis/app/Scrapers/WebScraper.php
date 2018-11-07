4950d509b2086698a8f634eef57cba38f23cac62
jordiprats <jordi.prats@gmail.com>
2018-11-07 21:16:54 +0100

    getHTMLByURL with caching

--- scubitis/app/Scrapers/WebScraper.php
+++ scubitis/app/Scrapers/WebScraper.php
@@ -7,23 +7,30 @@ abstract class WebScraper
   public abstract function getWebPriceByURL($url);
   public abstract function createProductByURL($url);
 
+  public $cached_urls = array();
+
   public function getHTMLByURL($url)
   {
-    $c = curl_init($url);
-    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($c, CURLOPT_USERAGENT,'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0');
-    //curl_setopt(... other options you want...)
+    if(!isset($cached_urls[$url]))
+    {
+      $c = curl_init($url);
+      curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($c, CURLOPT_USERAGENT,'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0');
+      //curl_setopt(... other options you want...)
 
-    $html = curl_exec($c);
+      $html = curl_exec($c);
 
-    if (curl_error($c))
-        die(curl_error($c));
+      if (curl_error($c))
+          die(curl_error($c));
 
-    // Get the status code
-    $status = curl_getinfo($c, CURLINFO_HTTP_CODE);
+      // Get the status code
+      $status = curl_getinfo($c, CURLINFO_HTTP_CODE);
 
-    curl_close($c);
+      curl_close($c);
 
-    return $html;
+      $cached_urls[$url] = $html;
+    }
+
+    return $cached_urls[$url];
   }
 }
