e57782141b240af293977750d25fa51ae3c1ff1e
jordiprats <jordi.prats@gmail.com>
2018-11-08 23:54:42 +0100

    video welcome

--- scubitis/resources/views/welcome.blade.php
+++ scubitis/resources/views/welcome.blade.php
@@ -14,8 +14,7 @@
                         </div>
                         {{ session()->forget('status') }}
                     @endif
-
-                    <h2></h2>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/DjO7U7az88s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
