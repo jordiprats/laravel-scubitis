5fac54e5e27dafb3d1c5f62a2fea0482178f4b96
jordiprats <jordi.prats@gmail.com>
2018-11-21 19:32:37 +0100

    estil search

--- scubitis/resources/views/layouts/search.blade.php
+++ scubitis/resources/views/layouts/search.blade.php
@@ -1,10 +1,12 @@
 {{ Form::open(['method' => 'GET', 'route' => ['products.search']]) }}
 {{-- https://stackoverflow.com/questions/10615872/bootstrap-align-input-with-button --}}
 <div class="form-group card">
-  <div class="card-body input-group">
-  {{ Form::text('q', '', ['size'=>'100x1', 'style' => 'width: 100%', 'class'=> 'form-control']) }}
+  <div class="card-body">
+    <div class="input-group">
+      {{ Form::text('q', '', ['size'=>'100x1', 'class'=> 'form-control']) }}
 
-  {{ Form::submit('Search', array('class'=>'input-group-btn btn-info btn-lg')) }}
+      <span class="input-group-append input-group-btn">{{ Form::submit('Search', array('class'=>'btn btn-default')) }}</span>
+    </div>
   {{ Form::close() }}
   </div>
 </div>
